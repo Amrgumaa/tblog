32edd1640790078a88917fc5967cd439d0ac0ef1
amrgomaa <amrgumaa@gmail.com>
2021-03-27 22:46:04 +0300

    appservie provieder adjusted commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    { if (! app()->runningInConsole()) {
         $categories = Category::take(5)->get();
         View::share('categories', $categories);
+    }
     }
 }
